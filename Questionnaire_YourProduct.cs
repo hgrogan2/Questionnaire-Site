3c0caa2b76d9775021202c51705a9a4eec30744f
hgrogan2 <49524756+hgrogan2@users.noreply.github.com>
2021-04-30 23:25:01 -0500

    Add files via upload
    I think I've got it mostly done, I'm just not sure about the UpdateProduct method because I'm not sure how to go about updating the product. Also, ignore the id at the top.

---
+++ Questionnaire_YourProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class YourProduct : ApiController
+{
+    List<Model.Product> products = new List<Model.Product>();
+    public int Id { get; set; }
+    [HttpGet]
+    public JsonResult<List<Models.Product>> GetAllProducts()
+    {
+        return products;
+    }
+    [HttpGet]
+    public JsonResult<Models.Product> GetProduct(int id)
+    {
+        var product = products.FirstOrDefault((p) => p.id == id);
+        if(product == null)
+        {
+            return DllNotFoundException();
+        }
+        return Ok(product);
+    }
+    [HttpPost]
+    public void InsertProduct(Models.Product product)
+    {
+        products.Add(product);
+    }
+    [HttpPut]
+    public void UpdateProduct(Models.Product product, int id)
+    {
+        product = GetProduct(id);
+        products.Update(product);
+    }
+    [HttpDelete]
+    public void DeleteProduct(int id)
+    {
+        Model.Product product = GetProduct(id);
+        products.Remove(product);
+        products.SaveChanges();
+    }
+}
